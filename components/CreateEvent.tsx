3ef11690e23b8cf6808393189ce55600b108477e
YourAverageMoron <ryannffc21@gmail.com>
2024-04-10 13:01:57 +0100

    remove some unused boilerplate

--- components/CreateEvent.tsx
+++ components/CreateEvent.tsx
@@ -1,10 +1,11 @@
-import { ProgressBar } from "@tremor/react";
+import { Metric, ProgressBar } from "@tremor/react";
 
 export default function CreateEvent() {
     return (
         <>
             <ProgressBar value={72} />
-            
+            <Metric>Start Time:</Metric>
+            <Metric>End Time:</Metric>
         </>
     );
 }
