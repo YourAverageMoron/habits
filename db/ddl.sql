f5fcfa0f9164f82454b52b8485919d2910ebea29
YourAverageMoron <ryannffc21@gmail.com>
2024-04-18 16:59:27 +0100

    add a new categories table for event tags

--- db/ddl.sql
+++ db/ddl.sql
@@ -8,14 +8,26 @@ CREATE TABLE events (
 	updated_at timestamp DEFAULT now()
 );
 
+ CREATE TABLE event_tag_categories (
+	id serial primary key,
+	name text NOT NULL,
+	created_at timestamp DEFAULT now(),
+	updated_at timestamp DEFAULT now()
+);
+
  CREATE TABLE event_tags (
 	id serial primary key,
-	event_id integer REFERENCES events (id),
-	category text NOT NULL,
+	event_id integer NOT NULL  REFERENCES events (id),
+	category_id integer NOT NULL  REFERENCES event_tag_categories (id),
 	value text NOT NULL,
 	created_at timestamp DEFAULT now(),
 	updated_at timestamp DEFAULT now()
 );
 
 CREATE UNIQUE INDEX  ON event_tags (id);
-CREATE UNIQUE INDEX  ON event_tags (category);
+CREATE UNIQUE INDEX  ON event_tags (category_id);
+
+ALTER TABLE events ENABLE ROW LEVEL SECURITY;
+ALTER TABLE event_tags ENABLE ROW LEVEL SECURITY;
+ALTER TABLE event_tag_categories ENABLE ROW LEVEL SECURITY;
+
