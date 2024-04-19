6964d2438280ad0479443b3e0b79b69e42a1fb0a
YourAverageMoron <ryannffc21@gmail.com>
2024-04-19 09:20:35 +0100

    Added row level security and user based access control to tables

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,5 +1,6 @@
 CREATE TABLE events (
 	id serial primary key,
+	user_id uuid default auth.uid(),
 	start_time integer NOT NULL,
 	end_time integer NOT NULL,
 	intesity integer NOT NULL,
@@ -8,15 +9,35 @@ CREATE TABLE events (
 	updated_at timestamp DEFAULT now()
 );
 
+ALTER TABLE events ENABLE ROW LEVEL SECURITY;
+CREATE POLICY user_access_events ON events
+    FOR ALL
+    USING ((select auth.uid()) = user_id);
+
+CREATE UNIQUE INDEX ON events(id);
+
+---
+
  CREATE TABLE event_tag_categories (
 	id serial primary key,
+	user_id uuid default auth.uid(),
 	name text NOT NULL,
 	created_at timestamp DEFAULT now(),
 	updated_at timestamp DEFAULT now()
 );
 
+ALTER TABLE event_tag_categories ENABLE ROW LEVEL SECURITY;
+CREATE POLICY user_access_event_tag_categories ON event_tag_categories
+    FOR ALL
+    USING ((select auth.uid()) = user_id);
+
+CREATE UNIQUE INDEX ON event_tag_categories(id);
+
+---
+
  CREATE TABLE event_tags (
 	id serial primary key,
+	user_id uuid default auth.uid(),
 	event_id integer NOT NULL  REFERENCES events (id),
 	category_id integer NOT NULL  REFERENCES event_tag_categories (id),
 	value text NOT NULL,
@@ -24,10 +45,12 @@ CREATE TABLE events (
 	updated_at timestamp DEFAULT now()
 );
 
-CREATE UNIQUE INDEX  ON event_tags (id);
-CREATE UNIQUE INDEX  ON event_tags (category_id);
-
-ALTER TABLE events ENABLE ROW LEVEL SECURITY;
 ALTER TABLE event_tags ENABLE ROW LEVEL SECURITY;
-ALTER TABLE event_tag_categories ENABLE ROW LEVEL SECURITY;
+CREATE POLICY user_access_event_tags ON event_tags
+    FOR ALL
+    USING ((select auth.uid()) = user_id);
+
+CREATE UNIQUE INDEX ON event_tags(id);
+CREATE INDEX ON event_tags(category_id);
+
 
