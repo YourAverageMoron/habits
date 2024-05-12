15c742014633b529e4a491f82c97fec488c1eb55
YourAverageMoron <ryannffc21@gmail.com>
2024-05-12 20:49:15 +0100

    we can now delete an event

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,16 +1,14 @@
-import { EventAccordionBody } from "@/components/EventAccordionBody";
 import Nav from "@/components/Nav";
 import { isLoggedIn } from "@/utils/supabase/checkLogin";
 import { createClient } from "@/utils/supabase/server";
-import { Accordion, AccordionBody, AccordionHeader, AccordionList, Badge } from "@tremor/react";
-import {formatDistanceToNow } from "date-fns";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
-import { EventAccordionBodyTags } from "@/components/EventAcordionBodyTags";
+import { EventsList } from "@/components/EventsList";
 
 
 // TODO:
 // PAGENTATE DATA PULL FROM SUPABASE -> CREATE AN INFINATE SCROLL
+// DELETE and UPDATE buttons 
 export default async function CreateEvent() {
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
@@ -28,13 +26,14 @@ export default async function CreateEvent() {
             , timezone
             , event_tags(
                 id
-                ,event_tag_categories(id, name)
-                ,category_id
-                ,value
+                , event_tag_categories(id, name)
+                , category_id
+                , value
             )
-            ,created_at`)
-        .order('created_at', { ascending: false })
-        .limit(1);
+            ,created_at
+            ,updated_at`)
+        .order('start_time', { ascending: false })
+        .range(0, 10);
 
 
     if (!events.data) {
@@ -47,43 +46,7 @@ export default async function CreateEvent() {
             <Nav />
             <div className="m-10">
                 <ol className="relative border-s border-tremor-border dark:border-dark-tremor-border">
-                    {events.data.map((d) => (
-                        <li className="mb-10 ms-6">
-                            <span className="absolute flex items-center justify-center w-6 h-6 bg-tremor-background rounded-full -start-3 ring-8 ring-tremor-background dark:ring-dark-tremor-background dark:bg-dark-tremor-background">
-                                <img className="rounded-full shadow-lg" src="/favicon.png" alt="Bonnie image" />
-                            </span>
-                            <AccordionList className="">
-
-                                <Accordion defaultOpen={true}>
-                                    <AccordionHeader className="text-xs font-medium text-tremor-content dark:text-dark-tremor-content">
-                                        <time className="mb-1 text-xs font-normal text-tremor-content-subtle dark:text-dark-tremor-content-subtle sm:order-last sm:mb-0">
-                                            Created {formatDistanceToNow(new Date(d.created_at))} ago
-                                        </time>
-                                    </AccordionHeader>
-                                    <AccordionBody className="leading-6">
-                                        <EventAccordionBody />
-                                    </AccordionBody>
-                                </Accordion>
-                                <Accordion>
-                                    <AccordionHeader className="text-xs font-medium text-tremor-content dark:text-dark-tremor-content">
-                                        Tags
-                                    </AccordionHeader>
-                                    <AccordionBody className="leading-6">
-                                        <EventAccordionBodyTags tags={d.event_tags} />
-                                    </AccordionBody>
-                                </Accordion>
-                                <Accordion>
-                                    <AccordionHeader className="text-xs font-medium text-tremor-content dark:text-dark-tremor-content">
-                                        Journal
-                                    </AccordionHeader>
-                                    <AccordionBody className="leading-6">
-                                        {d.journal}
-                                    </AccordionBody>
-                                </Accordion>
-                            </AccordionList>
-
-                        </li>
-                    ))}
+                    <EventsList events={events.data} />
                 </ol>
             </div>
         </>
--- components/EventAccordionBody.tsx
+++ components/EventAccordionBody.tsx
@@ -1,17 +1,31 @@
 "use client"
 import { Button } from "@tremor/react";
+import { formatInTimeZone } from "date-fns-tz";
 
 
-export function EventAccordionBody() {
+type EventAccordionBodyProps = {
+    eventId: number,
+    startTime: Date,
+    endTime: Date,
+    timezone: string,
+    intensity: number,
+    updateEvent: (id: number) => void,
+    deleteEvent: (id: number) => void,
+}
+export function EventAccordionBody(props: EventAccordionBodyProps) {
     return <div>
-        <p>Start Time - 1111</p>
-        <p>End Time - 1111</p>
-        <p>Intensity - 1</p>
+        <p>Start Time - {formatDateWithTimezone(props.startTime, props.timezone)}</p>
+        <p>End Time - {formatDateWithTimezone(props.endTime, props.timezone)}</p>
+        <p>Intensity - {props.intensity}</p>
         <div className="flex justify-center gap-3">
-            <Button onClick={() => console.log("qweqweqw")} size="sm" variant="secondary">Edit</Button>
-            <Button size="sm" color="red" variant="secondary">Delete</Button>
+            <Button onClick={() => props.updateEvent(props.eventId)} size="sm" variant="secondary">Edit</Button>
+            <Button onClick={() => props.deleteEvent(props.eventId)} size="sm" color="red" variant="secondary" >Delete</Button>
         </div>
-
     </div>
+
+}
+
+const formatDateWithTimezone = (d: Date, tz: string) => {
+    return formatInTimeZone(d, tz, 'yyyy-mm-dd HH:mm:ss zzz')
 
 }
